a5be455d4c5fff8e6c083300590b30fb63aba574
René Ruppert <rene@c-sharx.net>
2016-06-27 12:33:59 +0200

    Initial commit

---
+++ NotificationTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NotificationTest.Startup))]
+namespace NotificationTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
