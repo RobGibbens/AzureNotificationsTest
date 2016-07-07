2e74d98337fd484b76a99fa25a317fc5749b6c20
René Ruppert <rene@c-sharx.net>
2016-07-07 12:52:17 +0200

    Update client

---
+++ PushNotifications.iOS/CustomCell.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace PushNotifications.iOS
+{
+    public partial class CustomCell : UITableViewCell
+    {
+        public CustomCell (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
